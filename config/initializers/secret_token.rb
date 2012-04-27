ba7d45fef094203794888bab90305824e435161e
Yong Cha <yocha@yahoo.com>
2012-04-27 15:36:56 -0400

    first commit of agilebaby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agilebaby::Application.config.secret_token = '31361be2f0d2e8f7697807c5a373623bff53ae10c0100e9ca9c8bafdd485787751b7b760150be9643e295fc6112ef7c55ab0ce8684cfd7b329217957c3cf3f1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Agilebaby::Application.config.session_store :cookie_store, key: '_agilebaby_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Agilebaby::Application.config.session_store :active_record_store
