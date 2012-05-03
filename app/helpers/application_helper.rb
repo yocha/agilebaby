034e336159d72938f065e9b4298987393e7084c9
Yong Cha <yocha@yahoo.com>
2012-05-03 02:25:41 -0400

    link shopping item to amazon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def amazon_search_url(product)
+         raw   "<a href=\"http://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=#{product}\" target=\"_blank\" >#{product}</a>"
+  end
 end
